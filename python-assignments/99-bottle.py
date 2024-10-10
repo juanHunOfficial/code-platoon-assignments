b3fa74af03806674849cbaec855ed07601970045
JUAN_HUN <juanhun2424@gmail.com>
2024-10-10 14:57:59 -0400

    complete 99 bottles algorithim

--- python-assignments/99-bottle.py
+++ python-assignments/99-bottle.py
@@ -1,1 +1,8 @@
-print("hello world")
+bottles_of_beer = 99
+
+for beer in range(bottles_of_beer, 1, -1):
+    bottle = "bottles"
+    if (beer - 1 == 1):
+        bottle = "bottle" #take away the (s)
+    print(f"{beer} bottles of beer on the wall, {beer} bottles of beer. \n" + 
+          f"Take one down and pass it around, {beer -1} {bottle} of beer on the wall.")
