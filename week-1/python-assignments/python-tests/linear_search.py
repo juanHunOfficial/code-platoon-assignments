83935f231851ae808a82591695d2b9c991d845e6
JUAN_HUN <juanhun2424@gmail.com>
2024-10-11 11:28:22 -0400

    use for demos

--- week-1/python-assignments/python-tests/linear_search.py
+++ week-1/python-assignments/python-tests/linear_search.py
@@ -3,3 +3,5 @@ def linear_search(desired_num: int,             junk_arr: list)             -> i
     for index in range(len(junk_arr)):
         if junk_arr[index] == desired_num:
             return index
+    
+    return -1 #will only run if the for loop gets nothing
