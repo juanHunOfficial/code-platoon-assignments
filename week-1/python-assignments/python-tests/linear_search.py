70b7706665d6c8eedbf39a381e54e7b0fa8e6c2c
JUAN_HUN <juanhun2424@gmail.com>
2024-10-11 09:42:47 -0400

    update the comments to show the code in more detail

--- week-1/python-assignments/python-tests/linear_search.py
+++ week-1/python-assignments/python-tests/linear_search.py
@@ -1,6 +1,5 @@
-def linear_search(desired_num: int, junk_arr: list) -> int:
+def linear_search(desired_num: int,             junk_arr: list)             -> int: #syntax below:
+    #------------(parameter): expected pass     (parameter): expected pass  -> expected return value
     for index in range(len(junk_arr)):
         if junk_arr[index] == desired_num:
             return index
-
-print(f"See the test works fine with type annotation we got back: {linear_search(1, [3,2,4,1])}")
