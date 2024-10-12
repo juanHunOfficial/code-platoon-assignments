b224496b48aac309b82546dcce4f253b2681a2f9
JUAN_HUN <juanhun2424@gmail.com>
2024-10-12 12:36:36 -0400

    create and add Users class for the app, methods created include: updating user name and email, printing out the fully dict and a few test cases on the console

--- week-2/oop-intro/app-users-1.py
+++ week-2/oop-intro/app-users-1.py
@@ -1,15 +1,28 @@
-
+#profile have been made with _ because this is a private method
+_user_db = {} #key: "name" value: "name", "email", "license_number"
 
 class Users:
     
-    def __init__(self, name, email, license_number):
+    def __init__(self, name: str, email: str, license_number: str) -> None:
         self._name = name
         self._email = email
         self._license_number = license_number
+        _user_db.update({self._name: [self._name, self._email, self._license_number]}) #updating the master list in the program using the key above
         
-        self._user_db = {} #key: "name" value: "name", "email", "license_number"
-        self._user_db.update({self._name: [self._name, self._email, self._license_number]})
-        print(self._user_db)
+    def _print_full_users_db(self):
+        print(_user_db)
         
-temp_user1 = Users("Juan", "juan_hun@yahoo.com", "H500-555-555-55-5")
-temp_user2 = Users("Alyssa", "alyssanickole97@gmail.com", "H200-222-22-2")
+    def _update_user_email(self, name: str, new_email: str) -> None:
+        _user_db[name][1] = new_email
+        print("")
+
+    def _update_user_name(self, name: str, new_name: str) -> None:
+
+
+master_template = Users("Name", "template@code-platoon.com", "LXXX-XXX-XXX-XX-X")
+temp_user1 = Users("Juan Hun", "juan_hun@yahoo.com", "H500-555-555-55-5")
+temp_user2 = Users("Spongebob Squarepants", "bikini-bottom-chef@gmail.com", "H200-222-22-2")
+temp_user3 = Users("Patrick Theadore Star", "under-a-rock@yahoo.com", "H111-111-123-45-6")
+
+temp_user3._update_user_email("Patrick Theadore Star", "mayo_is_my_instrument@cheesecake.gov")
+master_template._print_full_users_db()
