23a8bf3a771310c1d6041afd21b48ae87597d6e4
JUAN_HUN <juanhun2424@gmail.com>
2024-10-14 12:10:46 -0400

    create algorithim that passed 5/5 tests for guessing_game.py

--- week-2/oop-intro/oop-guessing-game/guessing_game.py
+++ week-2/oop-intro/oop-guessing-game/guessing_game.py
@@ -1,3 +1,18 @@
 class GuessingGame():
-    # Write your code here
-    pass
+    def __init__(self, answer: int) -> None:
+        self.answer = answer
+    
+    def guess(self, guess: int) -> str:
+        self.guess = guess
+        if guess == self.answer:
+            return "correct"
+        elif guess > self.answer:
+            return "high"
+        else:
+            return "low"
+    
+    def solved(self):
+        if self.guess == self.answer:
+            return True
+        else: 
+            return False
