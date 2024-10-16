ab8a3c9c2338ce9739c2da59ed910966bbc854dc
JUAN_HUN <juanhun2424@gmail.com>
2024-10-16 16:10:29 -0400

    complete the User class, add the required methods to pass all tests as well as all getters and setters that are needed.

--- week-2/abrstraction-encapsulation/oop-app-users-ii/test_User.py
+++ week-2/abrstraction-encapsulation/oop-app-users-ii/test_User.py
@@ -4,16 +4,16 @@ from User import User
 user = User("John", "john@email.com", "FDUI87")
 
 def test_init_user():
-  assert user.Name == "John"
-  assert user.Email == "john@email.com"
-  assert user.Drivers_liscence == "FDUI87"
+  assert user.name == "John"
+  assert user.email == "john@email.com"
+  assert user.drivers_liscence == "FDUI87"
   
 def test_create_post(monkeypatch, capsys):
   input = iter(["Johns †i†le", "I Just joined OOPX"])
   monkeypatch.setattr("builtins.input", lambda _x: next(input))
   user.create_a_post()
   assert len(user.posts) == 1
-  assert len(user.User_posts) == 1
+  assert len(user.user_posts) == 1
   user.see_my_posts()
   out, err = capsys.readouterr()
   assert "Johns †i†le" in out
@@ -23,7 +23,7 @@ def test_delete_post(monkeypatch, capsys):
   monkeypatch.setattr("builtins.input", lambda _x: next(input))
   user.delete_a_post()
   assert len(user.posts) == 0
-  assert len(user.User_posts) == 0
+  assert len(user.user_posts) == 0
   user.see_my_posts()
   out, err = capsys.readouterr()
   assert out == ""
@@ -33,42 +33,42 @@ user3 = User(**{"name":"Zack", "email":"zack@email.com", "drivers_liscence":"FDU
   
 def test_see_all_posts(monkeypatch, capsys):
   assert len(user.posts) == 0
-  assert len(user.User_posts) == 0
+  assert len(user.user_posts) == 0
   user.see_all_posts()
   out, err = capsys.readouterr()
   assert out == ""
   input = iter(["Zack åttåck", "B1@ck H@t"])
   monkeypatch.setattr("builtins.input", lambda _x: next(input))
   user3.create_a_post()
-  assert len(user3.User_posts) == 1
+  assert len(user3.user_posts) == 1
   assert len(user.posts) == 1
   input = iter(["Mikes πost", "OOPX Founder"])
   monkeypatch.setattr("builtins.input", lambda _x: next(input))
   user2.create_a_post()
-  assert len(user2.User_posts) == 1
+  assert len(user2.user_posts) == 1
   assert len(user.posts) == 2
   input = iter(["Johns †i†le", "I Just joined OOPX"])
   monkeypatch.setattr("builtins.input", lambda _x: next(input))
   user.create_a_post()
-  assert len(user2.User_posts) == 1
+  assert len(user2.user_posts) == 1
   assert len(user.posts) == 3
 
 
 def test_see_my_posts(monkeypatch, capsys):
+  assert len(user.user_posts) == 1
   assert len(user.posts) == 3
-  assert len(user.User_posts) == 1
   input = iter(["Zack åttåck", "B1@ck H@t"])
   monkeypatch.setattr("builtins.input", lambda _x: next(input))
   user.create_a_post()
-  assert len(user.User_posts) == 2
+  assert len(user.user_posts) == 2
   assert len(user.posts) == 4
   input = iter(["Mikes πost", "OOPX Founder"])
   monkeypatch.setattr("builtins.input", lambda _x: next(input))
   user.create_a_post()
-  assert len(user.User_posts) == 3
+  assert len(user.user_posts) == 3
   assert len(user.posts) == 5
   input = iter(["Johns †i†le", "I Just joined OOPX"])
   monkeypatch.setattr("builtins.input", lambda _x: next(input))
   user.create_a_post()
-  assert len(user.User_posts) == 4
+  assert len(user.user_posts) == 4
   assert len(user.posts) == 6
