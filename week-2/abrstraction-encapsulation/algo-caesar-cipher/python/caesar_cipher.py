80c701e2384df6426828a8fde3e1435affa08934
JUAN_HUN <juanhun2424@gmail.com>
2024-10-18 13:00:38 -0400

    refactor the caesar cipher utilizing the built-in string method and other one-liners to shorten it from the original algorithm

--- week-2/abrstraction-encapsulation/algo-caesar-cipher/python/caesar_cipher.py
+++ week-2/abrstraction-encapsulation/algo-caesar-cipher/python/caesar_cipher.py
@@ -1,40 +1,24 @@
+import string
+
 def caesar_cipher(string_to_encrypt: str, shift_amount: int) -> None:
-    #making const-like variables to indicate the bounds for the letter ranges
-    LOWERCASE_UPPER_BOUND = 122
-    LOWERCASE_LOWER_BOUND = 97
-    
-    UPPERCASE_UPPER_BOUND = 90
-    UPPERCASE_LOWER_BOUND = 65
+    all_alpha = list(string.ascii_lowercase) + list(string.ascii_uppercase) 
     
     NUMBER_OF_LETTERS_IN_ALPHABET = 26
     
-    encrypted_message = ""# initialized as an empty string to append to
-    #for loop to iterate thru the string and check the ascii values of the characters
+    encrypted_message = ""
+    
     for char in string_to_encrypt:
         
-    #using conditional statments to check if it is a capital letter, lowercase, or if we should skip and just append
-    #lowercase in regular instances will be most common therefor it is checked for first    
-        # if LOWERCASE_LOWER_BOUND <= ord(char) <= LOWERCASE_UPPER_BOUND:
-        if char.islower():
-            ###is alpha[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+        if char.isalpha():
+            index = all_alpha.index(char)
+            new_char = (index + shift_amount) % NUMBER_OF_LETTERS_IN_ALPHABET
             
-            if (ord(char) + shift_amount) > LOWERCASE_UPPER_BOUND: 
-                char = chr((ord(char) + shift_amount) - NUMBER_OF_LETTERS_IN_ALPHABET)
-            elif (ord(char) + shift_amount) < LOWERCASE_LOWER_BOUND:
-                char = chr((ord(char) + shift_amount) + NUMBER_OF_LETTERS_IN_ALPHABET)
+            if char.isupper():
+                encrypted_message += all_alpha[new_char + NUMBER_OF_LETTERS_IN_ALPHABET]
             else:
-                char = chr(ord(char) + shift_amount)
-
-        # elif UPPERCASE_LOWER_BOUND <= ord(char) <= UPPERCASE_UPPER_BOUND:
-        if char.isupper():
-
-            if (ord(char) + shift_amount) > UPPERCASE_UPPER_BOUND: 
-                char = chr((ord(char) + shift_amount) - NUMBER_OF_LETTERS_IN_ALPHABET)
-            elif (ord(char) + shift_amount) < UPPERCASE_LOWER_BOUND:
-                char = chr((ord(char) + shift_amount) + NUMBER_OF_LETTERS_IN_ALPHABET)
-            else:
-                char = chr(ord(char) + shift_amount)
+                encrypted_message += all_alpha[new_char]
+                
+        else:
+            encrypted_message += char
             
-        encrypted_message += char
     return encrypted_message
-    
